01748467f5493f11de04c30e396d69dffa29fd33
Murat Mayadağ <murat-2@hotmail.com>
2020-02-21 16:03:14 +0300

    Initial Commit

---
+++ Objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-c
+//
+//  Created by Murat Mayadağ on 21.02.2020.
+//  Copyright © 2020 Murat Mayadağ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
